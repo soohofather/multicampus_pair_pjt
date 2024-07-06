1c8156e38c997094fc1a4e4f07d2d93a39130c26
이동현 <enthchal@gmail.com>
2024-07-06 23:35:31 +0900

    Squashed 'django_1014/' content from commit b9b51f8
    git-subtree-dir: django_1014
    git-subtree-split: b9b51f8631f2c77282548b84e7d4b3901cfc5545

---
+++ reviews/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'reviews'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('create', views.create, name='create'),
+    path('detail/<int:pk>', views.detail, name='detail'),
+    path('update/<int:pk>', views.update, name='update'),
+    path('delete/<int:pk>', views.delete, name='delete'),
+]
